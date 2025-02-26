19e91e26f8ae6ef4d317f11c951f41c6ff1fd675
julien.bouin.etu <julien.bouin.etu@univ-lille.fr>
2025-02-26 14:33:38 +0100

    ajout competences

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,56 +1,57 @@
 <script>
-    export let showProfile;
+  export let showProfile;
 </script>
 
 <div class="header">
-    {#if showProfile == "true"}
-        <img
-            src="/face.png"
-            alt=""
-            width="40"
-            height="auto"
-            class="header__profile-img"
-        />
-    {/if}
-    <nav class="nav">
-        <a href="/" alt="">Home</a>
-        <a href="/projects" alt="">Projets</a>
-        <a href="/exp" alt="">Experiences</a>
-    </nav>
+  {#if showProfile == "true"}
+    <img
+      src="/face.png"
+      alt=""
+      width="40"
+      height="auto"
+      class="header__profile-img"
+    />
+  {/if}
+  <nav class="nav">
+    <a href="/" alt="">Home</a>
+    <a href="/projects" alt="">Projets</a>
+    <a href="/competences" alt="">Compétences</a>
+    <a href="/exp" alt="">Experiences</a>
+  </nav>
 </div>
 
 <style>
-    .header {
-        position: relative;
-        padding-top: 30px;
-        margin-bottom: 7%;
-        font-size: 1.1em;
-        text-align: center;
-        align-items: center;
-    }
+  .header {
+    position: relative;
+    padding-top: 30px;
+    margin-bottom: 7%;
+    font-size: 1.1em;
+    text-align: center;
+    align-items: center;
+  }
 
-    .header__profile-img {
-        position: absolute;
-        left: -24%;
-        top: 82%;
-        transform: translateY(-50%);
-        border: 1px solid white;
-        border-radius: 10px;
-    }
+  .header__profile-img {
+    position: absolute;
+    left: -24%;
+    top: 82%;
+    transform: translateY(-50%);
+    border: 1px solid white;
+    border-radius: 10px;
+  }
 
-    .nav {
-        display: inline-block;
-    }
+  .nav {
+    display: inline-block;
+  }
 
-    nav a {
-        margin: 0 10px;
-        color: white;
-        text-decoration: none;
-    }
+  nav a {
+    margin: 0 10px;
+    color: white;
+    text-decoration: none;
+  }
 
-    nav a:hover {
-        filter: drop-shadow(0 0 6mm white);
-        color: #c5d4de;
-        transition: 0.2s;
-    }
+  nav a:hover {
+    filter: drop-shadow(0 0 6mm white);
+    color: #c5d4de;
+    transition: 0.2s;
+  }
 </style>
---
+++ src/routes/competences/+page.svelte
@@ -0,0 +1,89 @@
+<script>
+  import Experience from "../../components/experience.svelte";
+</script>
+
+<div class="introduction">
+  <h1>Compétences</h1>
+  <p>Voici un aperçu de mes compétences jusqu'à présent :</p>
+</div>
+<div class="competences">
+  <div class="competence">
+    <h2>Java</h2>
+    <p>
+      Langage orienté objet utilisé pour le développement d'applications
+      robustes.
+    </p>
+  </div>
+  <div class="competence">
+    <h2>SQL</h2>
+    <p>Langage de requête pour manipuler et interroger des bases de données.</p>
+  </div>
+  <div class="competence">
+    <h2>JavaScript</h2>
+    <p>
+      Langage de programmation essentiel pour le développement web dynamique.
+    </p>
+  </div>
+  <div class="competence">
+    <h2>React</h2>
+    <p>
+      Bibliothèque JavaScript pour créer des interfaces utilisateur
+      interactives.
+    </p>
+  </div>
+  <div class="competence">
+    <h2>Vue.js</h2>
+    <p>
+      Framework JavaScript progressif pour construire des interfaces modernes.
+    </p>
+  </div>
+  <div class="competence">
+    <h2>Flutter</h2>
+    <p>Framework de développement d'applications mobiles multiplateformes.</p>
+  </div>
+  <div class="competence">
+    <h2>C</h2>
+    <p>Langage de programmation bas niveau connu pour sa performance.</p>
+  </div>
+  <div class="competence">
+    <h2>Python</h2>
+    <p>Langage polyvalent apprécié pour sa simplicité et sa puissance.</p>
+  </div>
+  <div class="competence">
+    <h2>Spring</h2>
+    <p>
+      Framework Java pour créer des applications web robustes et évolutives.
+    </p>
+  </div>
+</div>
+
+<style>
+  .introduction {
+    width: 60vw;
+    font-weight: 300;
+    font-size: 1.1em;
+  }
+
+  .introduction > h1 {
+    font-size: 3em;
+    font-weight: 600;
+  }
+
+  .competences {
+    margin-top: 7%;
+    width: 60vw;
+  }
+
+  .competence {
+    margin-bottom: 2%;
+  }
+
+  .competence > h2 {
+    font-size: 1.5em;
+    font-weight: 500;
+  }
+  p {
+    font-weight: 300;
+    opacity: 0.7;
+  }
+</style>
